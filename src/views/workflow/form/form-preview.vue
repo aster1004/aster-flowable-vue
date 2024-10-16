051c25da0e83da03d3a0be312eea5ff83a4d1168
Aster <lipian1004@163.com>
2024-10-16 10:14:17 +0800

    style: 去除表单预览中的确定按钮

--- src/views/workflow/form/form-preview.vue
+++ src/views/workflow/form/form-preview.vue
@@ -24,7 +24,6 @@
       :form-info="_formInfo"
     />
     <template #footer>
-      <el-button type="primary" @click="validateForm">{{ $t('button.confirm') }}</el-button>
       <el-button @click="visible = false">{{ $t('button.cancel') }}</el-button>
     </template>
   </el-drawer>
@@ -75,13 +74,6 @@
   });
 
   /**
-   * @description: 验证表单
-   */
-  const validateForm = () => {
-    formRenderRef.value.validate();
-  };
-
-  /**
    * @description: 表单数据
    */
   const _formData = computed(() => {
@@ -90,8 +82,8 @@
 
   watch(
     () => _formData.value,
-    (val, oval) => {
-      console.log('preview--->', val, oval);
+    (val) => {
+      console.log('preview--->', val);
     },
     {
       immediate: true,
